51f58c21a0127b38c2a2b5f6d0cfe377e3b7d9b3
MonyKazu <0av6w84t38t580w@au.com>
2024-05-10 17:22:35 +0900

    削除機能の追加

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @tweets = Tweet.all.order(created_at: "DESC")
+      @new_tweet = current_user.tweets.new
     end
   end
 end
