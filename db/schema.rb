af9f4ee051229cce9c516dea2eb7d82e3e794074
MonyKazu <0av6w84t38t580w@au.com>
2024-05-15 13:00:26 +0900

    最初のコミット

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_08_050611) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_15_033505) do
+  create_table "follow_relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_follow_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_follow_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_follow_relationships_on_follower_id"
+  end
+
+  create_table "tweets", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_tweets_on_user_id_and_created_at"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "uid"
     t.string "nickname"
